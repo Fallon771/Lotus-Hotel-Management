4d6b37e4c00599dc80df9b822feaaebb5c85abc6
Fallon771 <33718310+Fallon771@users.noreply.github.com>
2018-01-30 20:42:55 +0000

    Guest Commit

---
+++ src/ie/lyit/users/Guest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ie.lyit.users;
+
+/**
+ *
+ * @author Jim
+ */
+public class Guest {
+    
+    private static int guestNumber;
+    private int roomNumber;
+    private String address;
+    Name name;
+    
+    public Guest(){
+    }
+    
+    
+    public String toString(){
+    return "";
+    }
+}
