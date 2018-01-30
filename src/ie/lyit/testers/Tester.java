0f676ed2ecec424e99f8427ec00b68a4cf02c06d
Fallon771 <33718310+Fallon771@users.noreply.github.com>
2018-01-30 20:43:09 +0000

    Tester commit

---
+++ src/ie/lyit/testers/Tester.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ie.lyit.testers;
+
+/**
+ *
+ * @author Jim
+ */
+public class Tester {
+    
+    public Tester(){
+    
+    }
+    public static void main(String args[]){
+        
+        System.out.print("Test class");
+    
+    }    
+}
+
