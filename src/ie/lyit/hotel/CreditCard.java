9ee183384ca3a1be8b794faf11240da593b9b474
JamesF <jamesafallon@yahoo.ie>
2018-02-07 22:38:05 +0000

    Credit card class created.

---
+++ src/ie/lyit/hotel/CreditCard.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ie.lyit.hotel;
+
+/**
+ *
+ * @author jim
+ */
+public class CreditCard {
+    
+    private int cardNum;
+    private int cvc;
+    
+    private String expiry;
+    private String fName;
+    private String sName;
+    
+    
+    public CreditCard(String fName,String sName,int cardNum,String expiry,int cvc){
+        this.fName = fName;
+        this.sName = sName;
+        this.cardNum = cardNum;
+        this.expiry = expiry;
+        this.cvc = cvc;
+    }
+    public void addCard(){
+    
+    }
+    public void removeCard(){
+    
+    }
+    // Getters & Setter's
+    public int getCardNum() {
+        return cardNum;
+    }
+
+    public void setCardNum(int cardNum) {
+        this.cardNum = cardNum;
+    }
+
+    public int getCvc() {
+        return cvc;
+    }
+
+    public void setCvc(int cvc) {
+        this.cvc = cvc;
+    }
+
+    public String getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(String expiry) {
+        this.expiry = expiry;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+    
+}
