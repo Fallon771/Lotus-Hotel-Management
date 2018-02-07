eafc28a706f98141dfe372ac5f8ab0e3f55c2aec
JamesF <jamesafallon@yahoo.ie>
2018-02-07 19:35:36 +0000

    Search class added.

---
+++ src/ie/lyit/hotel/Search.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ie.lyit.hotel;
+
+/**
+ *
+ * @author jim
+ */
+public class Search {
+    
+    private String guestName;
+    private int roomNum;
+    private Date date;
+    
+    public Search(){
+        
+    this.guestName = null;
+    this.date = null;
+    
+    }
+    public void searchRoom(){
+    
+    }
+    public void searchGuest(){
+  
+    }
+    public void searchDate(){
+    
+    }
+}
