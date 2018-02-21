5cfeccd26ffaa8018df9358bad0926496a60f3c7
JamesF <jamesafallon@yahoo.ie>
2018-02-21 21:05:46 +0000

    Removed bookRoom();

--- src/ie/lyit/database/DBConnections.java
+++ src/ie/lyit/database/DBConnections.java
@@ -5,7 +5,7 @@
  */
 package ie.lyit.database;
 import ie.lyit.users.Guest;
-
+import ie.lyit.hotel.Rooms;
 /**
  *
  * @author james fallon
@@ -17,5 +17,4 @@ public interface DBConnections {
     public void removeGuest();
     public void displayGuests();
     public void displayRooms();
-    public void bookRoom();
 }
