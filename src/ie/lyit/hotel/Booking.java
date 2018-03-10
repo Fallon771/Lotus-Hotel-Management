aebfbe278015ade750257326b52a977ddf19d91c
Fallon771 <fallonjames32@gmail.com>
2018-03-10 09:58:13 +0000

    Added fields for Booking class -- Added getter's

--- src/ie/lyit/hotel/Booking.java
+++ src/ie/lyit/hotel/Booking.java
@@ -6,8 +6,25 @@ package ie.lyit.hotel;
  */
 public class Booking {
     
+    Date checkout,checkin;
+    int id,adults,children;
+    
+    
     public Booking(int id,Date checkin,Date checkout,int children,int adults){
         
+        this.id = id;
+        this.checkin =checkin;
+        this.checkout = checkout;
+        this.children =children;
+        this.adults = adults;
         
     }  
+    
+    public Date getCheckIn(){
+        
+        return checkin;
+    }
+    public Date getCheckOut(){
+        return checkout;
+    }
 }
