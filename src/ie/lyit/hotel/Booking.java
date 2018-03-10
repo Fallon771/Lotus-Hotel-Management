f598f31dd82e6958167a6a5155b03e51c6afa295
Fallon771 <fallonjames32@gmail.com>
2018-03-10 08:33:33 +0000

    Fixing code for updated database -- Created new booking class

---
+++ src/ie/lyit/hotel/Booking.java
@@ -0,0 +1,13 @@
+package ie.lyit.hotel;
+
+/**
+ *
+ * @author James Fallon
+ */
+public class Booking {
+    
+    public Booking(int id,Date checkin,Date checkout,int children,int adults){
+        
+        
+    }  
+}
